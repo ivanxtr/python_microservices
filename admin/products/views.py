8e6344f16a328a5e4b0ab985b11a1b64a3821160
Ivan Rodriguez <sbr.com@hotmail.com>
2020-12-22 23:30:46 -0600

    setting up rest api

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+  def list(self, request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+  def create(self, request):
+    pass
+
+  def retrieve(self, request, pk=None):
+    pass
+
+  def update(self, request, pk=None):
+    pass
+
+  def destroy(self, request, pk=None):
+    pass
