a317ef7404148308ed51da947ac5d257b9b0f86c
Ivan Rodriguez <sbr.com@hotmail.com>
2020-12-23 00:02:50 -0600

    more changes

--- admin/products/urls.py
+++ admin/products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductViewSet
+from .views import ProductViewSet, UserAPIView
 
 urlpatterns = [
     path('products', ProductViewSet.as_view({
@@ -10,6 +10,6 @@ urlpatterns = [
       'get': 'retrieve',
       'put': 'update',
       'delete': 'destroy'
-    }))
+    })),
+    path('user', UserAPIView.as_view())
 ]
-
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
-from .models import Product
+from rest_framework.views import APIView
+from .models import Product, User
 from .serializers import ProductSerializer
+import random
 
 class ProductViewSet(viewsets.ViewSet):
   def list(self, request):
@@ -10,13 +12,32 @@ class ProductViewSet(viewsets.ViewSet):
     return Response(serializer.data)
 
   def create(self, request):
-    pass
+    serializer = ProductSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
   def retrieve(self, request, pk=None):
-    pass
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
   def update(self, request, pk=None):
-    pass
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(instance=product, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
   def destroy(self, request, pk=None):
-    pass
+    product = Product.objects.get(id=pk)
+    product.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserAPIView(APIView):
+  def get(self, _): 
+    users = User.objects.all()
+    user = random.choice(users)
+    return Response({
+      'id': user.id
+    })
